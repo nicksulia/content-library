ceb47046778b1e2f5d2614fc18ffec24c27b5c86
Nick <nicksuliava@gmail.com>
2018-01-02 21:16:39 +0200

    config extended

--- serverConfig.js
+++ serverConfig.js
@@ -1,5 +1,7 @@
-export default {
+export default Object.freeze({
     'secret': 'allyourbasearebelongstous',
     'database': 'mongodb://localhost:27001/testDB',
-    'port': '8009'
-}
+    'port': '8009',
+    'portDB': '27001',
+    'hostDB':'127.0.0.1'
+});
