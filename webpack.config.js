ec7df5bec81734bb90a9cee4acd52dba4c0e1cc4
nicksulia <nicksuliava@gmail.com>
2018-01-08 23:33:50 +0200

    added new modules, webpack config extended with new rules and plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,49 @@
 const path = require('path');
-//const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const UglifyJsPlugin = require('webpack').optimize.UglifyJsPlugin;
+const CompressionPlugin = require("compression-webpack-plugin");
+
+const isProduction = process.env.NODE_ENV === "production";
+
+let plugins;
+if (isProduction) {
+    plugins = [
+        new ExtractTextPlugin({ filename: 'style.css', allChunks: true }),
+        new UglifyJsPlugin({
+            compress: {
+                sequences: false
+            },
+            output: {
+                semicolons: false
+            },
+            sourceMap: false
+        }),
+        new CopyWebpackPlugin([{
+            from: 'client-src/index.html',
+            to: './',
+            flatten: true
+        }])
+    ]
+} else {
+    plugins = [
+        new ExtractTextPlugin({ filename: 'style.css', allChunks: true }),
+        new CopyWebpackPlugin([{
+            from: 'client-src/index.html',
+            to: './',
+            flatten: true
+        }])
+    ]
+}
 
 module.exports = {
-    entry: path.resolve(__dirname, './src/index.js'),
+    entry: path.resolve(__dirname, './client-src/init.js'),
     output: {
         path: path.resolve(__dirname, 'dist'),
         publicPath: "/assets/",
         filename: 'bundle.js'
     },
-    devtool: 'source-map',
+    devtool: isProduction ? 'none' : 'source-map',
     devServer: {
         port: 8090,
         disableHostCheck: true
@@ -31,8 +66,21 @@ module.exports = {
                 use: ['style-loader', 'css-loader']
             },
             {
-                test: /\.s(a|c)ss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
+                test: /\.scss$/,
+                include: [path.resolve(__dirname, './client-src/components/')],
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [{
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            localIdentName: '[path]___[name]__[local]___[hash:base64:5]',
+                        },
+                    },
+                        { loader: 'resolve-url-loader', options: { sourceMap: true } },
+                        { loader: 'postcss-loader', options: { sourceMap: true } },
+                        { loader: 'sass-loader', options: { sourceMap: true } }],
+                }),
             },
             {
                 test: /\.svg$/,
@@ -40,4 +88,5 @@ module.exports = {
             }
         ]
     },
+    plugins: plugins
 };
