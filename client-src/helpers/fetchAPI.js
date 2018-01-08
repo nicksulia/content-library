1bbb6eea3ebaaaa3fd64920cf830d1d62010c5c0
nicksulia <nicksuliava@gmail.com>
2018-01-08 19:40:16 +0200

    server API created

---
+++ client-src/helpers/fetchAPI.js
@@ -0,0 +1,46 @@
+'use strict';
+
+function fetchAPI(url, options = {}) {
+    options.credentials = 'same-origin';
+    options.headers =  {
+        ...(options.headers),
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+    };
+
+    return fetch(url, options)
+        .then(res => {
+            if (!res.ok) {
+                return res.json().then(text => Promise.reject(text));
+            }
+            return res.text()
+                .then(text => {
+                    if (text) {
+                        console.log(JSON.parse(text));
+                        return JSON.parse(text);
+                    }
+                });
+        });
+}
+
+fetchAPI.get = (url, options) => {
+    return fetchAPI(url, options);
+};
+
+fetchAPI.post = (url, body, options={}) => {
+    return fetchAPI(url, {
+        ...options,
+        method: 'POST',
+        body: JSON.stringify(body)
+    });
+};
+
+fetchAPI.put = (url, body, options={}) => {
+    return fetchAPI(url, {
+        ...options,
+        method: 'PUT',
+        body: JSON.stringify(body)
+    });
+};
+
+export default fetchAPI;
