46f2f203b133a2f6651e65a86c0a6b71aca2cbef
nicksulia <nicksuliava@gmail.com>
2018-01-08 23:34:20 +0200

    added postcss config

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    plugins: {
+        'autoprefixer': {
+            browsers: [
+                'last 2 Chrome versions',
+                'last 2 Safari versions',
+            ],
+            sourceMap:	true
+        }
+    }
+};
