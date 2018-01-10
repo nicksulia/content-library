f805456a6ba480fc9f6e16f5c4caf3cb5307d50b
nicksulia <nicksuliava@gmail.com>
2018-01-10 18:20:38 +0200

    DB Methods extended with some init validation. Overhead fixed

--- dbMethods.js
+++ dbMethods.js
@@ -5,7 +5,7 @@ const collection = config.collectionName;
 const database = config.databaseName;
 
 export const find = ({ searchBy = {}, sortBy = "", filter = {type: ""}, cursor = 1 }) => {
-
+    //TODO: validate params before using it and return proper errors if validation is not passed
     return new Promise((res, rej) => {
         MongoClient.connect(url, (err, client) => {
             if (err) {
@@ -20,7 +20,8 @@ export const find = ({ searchBy = {}, sortBy = "", filter = {type: ""}, cursor =
             //limitations and sort options as variable to prevent code duplication
             const options =
                 {
-                    limit: 1000*(cursor || 1),
+                    skip: 1000*(cursor ? (cursor - 1) : 0) || 0, //case NaN returns 0
+                    limit: 1000, //hardcoded response limitation
                     sort: sortBy ? {[sortBy]:-1} : {_id:1}
                 };
 
@@ -28,6 +29,7 @@ export const find = ({ searchBy = {}, sortBy = "", filter = {type: ""}, cursor =
             const collectionCallback = (err, result) => {
                 if (err) {
                     console.log('Error in collection callback', err);
+                    //TODO: create proper error responses for different types of errors
                     rej({err, client});
                 }
                 res({result, client});
@@ -38,7 +40,7 @@ export const find = ({ searchBy = {}, sortBy = "", filter = {type: ""}, cursor =
                         db.collection(collection).find(
                             Object.assign({},{
                                 modified: {
-                                    $gte: filter.options.from ? new Date(filter.options.from) : new Date(2000, 1, 1),
+                                    $gte: filter.options.from ? new Date(filter.options.from) : new Date(2000, 1, 1), //if empty, returns (min, max)
                                     $lte: filter.options.to ? new Date(filter.options.to) : new Date(2040, 1, 1),
                                 }
                             },searchBy),
@@ -50,10 +52,10 @@ export const find = ({ searchBy = {}, sortBy = "", filter = {type: ""}, cursor =
                         db.collection(collection).find(
                             Object.assign({},{
                                 value: {
-                                    $gte: filter.options.from ? parseFloat(filter.options.from) : -Infinity,
-                                    $lte: filter.options.to ? parseFloat(filter.options.to) : Infinity,
+                                    $gte: filter.options.from !== undefined ? parseFloat(filter.options.from) : -Infinity, //if empty, returns (min, max)
+                                    $lte: filter.options.to !== undefined ? parseFloat(filter.options.to) : Infinity,
                                 },
-                                currency: filter.options.currency ? filter.options.currency : 'USD'
+                                currency: filter.options.currency ? filter.options.currency : 'USD' //default currency
                             },searchBy),
                             options
                         )
