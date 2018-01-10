8489d208def544a69fa3a8511d82460f7d7d711a
nicksulia <nicksuliava@gmail.com>
2018-01-10 18:10:38 +0200

    server calls refactored

--- client-src/api/configAPI.js
+++ client-src/api/configAPI.js
@@ -2,6 +2,41 @@
 
 import fetchAPI from '../helpers/fetchAPI.js'
 
+const freeAPI = '/apis';
+const register = '/register';
+const authenticate = '/api/authenticate';
+const securedAPI = '/api/findData';
+const checkAPI = '/api/check';
+
+export const getFreeDataApi = (body) => {
+    return fetchAPI.post(freeAPI, body)
+};
+
+export const checkAccess = () => {
+    return fetchAPI.get(checkAPI,{
+        headers:{
+            'Authorization': `Bearer ${sessionStorage.token}`
+        },
+    })
+};
+
 export const getDataApi = (body) => {
-    return fetchAPI.post('/apis', body)
-}
+    if (sessionStorage.token) {
+        return fetchAPI.post(securedAPI, body, {
+            headers:{
+                'Authorization': `Bearer ${sessionStorage.token}`
+            },
+        })
+    } else return Promise.reject({
+        code: 'no.saved.token',
+        message: 'No token provided. Please register and authenticate first.'
+    })
+};
+
+export const authenticateUser = (body) => {
+    return fetchAPI.post(authenticate, body);
+};
+
+export const registerUser = () => {
+    return fetchAPI.get(register);
+};
