fe8e718c0c933e68f2b11ea634de204051a93299
nicksulia <nicksuliava@gmail.com>
2018-01-11 14:54:18 +0200

    removed promise middleware

--- client-src/store/index.js
+++ client-src/store/index.js
@@ -2,9 +2,8 @@
 
 import { applyMiddleware, createStore, compose } from 'redux';
 import thunk from 'redux-thunk';
-import promiseMiddleware from 'redux-promise-middleware';
 import rootReducer from '../reducers';
 
-const store = compose(applyMiddleware(thunk, promiseMiddleware()))(createStore)(rootReducer);
+const store = compose(applyMiddleware(thunk))(createStore)(rootReducer);
 
 export default store;
